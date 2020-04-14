59cb03d0987619f29dc7d9a9a9535a94b19ce263
Shadia Ali <shadiamalfoy@gmail.com>
2020-04-13 20:24:29 -0400

    omg its werking

--- resources/views/vendor/chatter/home.blade.php
+++ resources/views/vendor/chatter/home.blade.php
@@ -94,20 +94,17 @@
 					        		<div class="chatter_middle">
 									
 										
-										<h3 class="chatter_middle_title"> title:{{ $discussion->title }} <div class="chatter_cat" style="background-color:{{ $discussion->category->color }}">{{ $discussion->category->name }}</div></h3>
+										<h3 class="chatter_middle_title"> {{ $discussion->character->character_name }} title:{{ $discussion->title }} <div class="chatter_cat" style="background-color:{{ $discussion->category->color }}">{{ $discussion->category->name }}</div></h3>
 					
 				
 										
 		<span class="chatter_middle_details">@lang('chatter::messages.discussion.posted_by') 
 			
-			@foreach($characters as $character)
 			
-			
-			@endforeach
 			
 			<span data-href="character/{{ $discussion->current_character_id }}">profile</span>
 
-
+			
 		<span data-href="/user">{{ ucfirst($discussion->user->{Config::get('chatter.user.database_field_with_user_name')}) }}</span> {{ \Carbon\Carbon::createFromTimeStamp(strtotime($discussion->created_at))->diffForHumans() }}</span>
 					        			@if($discussion->post[0]->markdown)
 					        				<?php $discussion_body = GrahamCampbell\Markdown\Facades\Markdown::convertToHtml( $discussion->post[0]->body ); ?>
