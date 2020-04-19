91bfc274109e7d4a9fcdf3d381fe08894e89e639
Shadia Ali <shadiamalfoy@gmail.com>
2020-04-19 10:09:15 -0400

    welcome pg styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -6,101 +7,153 @@
 
         <title>Laravel</title>
 
+        <!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
+
+            html,
+            body,
+            header,
+            .view {
+              height: 100%;
+            }
+          
+            @media (min-width: 320px) {
+              html,
+              body,
+              header,
+              .view {
                 height: 100vh;
-                margin: 0;
+              }
             }
-
-            .full-height {
-                height: 100vh;
+          
+            .top-nav-collapse {
+              background-color: #78909c !important;
             }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
+          
+            .navbar:not(.top-nav-collapse) {
+              background: transparent !important;
             }
-
-            .position-ref {
-                position: relative;
+          
+            @media (max-width: 991px) {
+             .navbar:not(.top-nav-collapse) {
+              background: #78909c !important;
+             }
             }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
+            
+            h1 {
+              letter-spacing: 8px;
             }
-
-            .content {
-                text-align: center;
+          
+            h5 {
+              letter-spacing: 3px;
             }
-
-            .title {
-                font-size: 84px;
+          
+            .hr-light {
+              border-top: 3px solid #fff;
+              width: 80px;
             }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+          
+          </style>
+          
+          <!-- Main navigation -->
+        
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+    <div class="container">
+      
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7"
+        aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="images/rpgLabLogo.svg" style="width:9em;"></a>
+      
+      <div class="collapse navbar-collapse text-center" id="navbarSupportedContent-7">
+        <ul class="navbar-nav ml-auto">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="rules">Rules</a>
-                    <a href="gameinfo">Game Info</a>
-                    <a href="characterclasses">Character Classes</a>
+          <li class="nav-item">
+            <a class="nav-link" href="rules">Rules</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="gameinfo">Game Info</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="characterclasses">Character Classes</a>
+          </li>
+          @auth
+          <li class="nav-item">
+            <a class="nav-link" href="{{ url('/home') }}">Home</a>
+          </li>
+          @else
 
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                    
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">Login</a>
+          </li>
+          @if (Route::has('register'))
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">Register</a>
+          </li>
+          @endif
+            @endauth
+    @endif
+        </ul>
+        
+      </div>
+      
+    </div>
+  </nav>
+  <!-- Navbar -->
 
-                        <a href="{{ route('login') }}">Login</a>
+<header>
+    <body>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<!-- Full Page Intro -->
+<div class="view jarallax" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/img%20%2848%29.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+      <!-- Content -->
+      <div class="container">
+        <!--Grid row-->
+        <div class="row">
+          <!--Grid column-->
+          <div class="col-12 mb-4 white-text text-center">
+            <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown"
+              data-wow-delay="0.3s"><strong>RpgLab</strong></h1>
+            <hr class="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s">
+            <h5 class="text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>A Play-by-Post RPG Creator</strong></h5>
+            <h5>rpgLab is a web platform based on online forum play-by-post role-playing games - but customized to fit the needs of the game master and the game.</h5>
+            <a href="http://shadiaali.ca" target="_blank" class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">My Portfolio</a>
+            <a href="http://shadiaali.ca/thesis" target="_blank" class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">Thesis Mini-Site</a>
+          </div>
+          <!--Grid column-->
         </div>
+        <!--Grid row-->
+      </div>
+      <!-- Content -->
+    </div>
+    <!-- Mask & flexbox options-->
+  </div>
+  <!-- Full Page Intro -->
+</header>
+<!-- Main navigation -->
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/js/mdb.min.js"></script>
     </body>
 </html>
