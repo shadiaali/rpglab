dfee9299ef88cb8ec3c4a0edfed5dfe360b20b1c
Shadia Ali <shadiamalfoy@gmail.com>
2020-04-20 02:12:51 -0400

    finished character dash

--- resources/views/myCharacters.blade.php
+++ resources/views/myCharacters.blade.php
@@ -5,15 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-11">
             <div class="card">
-                <div class="card-header">Character Dashboard</div>
-
+                <div class="card-header">Character Dashboard </div>
+                <p class="text-right"><a class="text-white btn btn-lg btn-yellow" href="addcharacter" role="button">Create A Character</a>
+                    
+                    <a class="text-white btn btn-lg btn-light-blue" href="characters" role="button">Character Directory</a></p>
                 <div class="card-body">
                     
                     
                     
             
             <section class="p-md-3 mx-md-5 text-center">
-                    
+                    <h4>My Characters</h4>
                     <div class="row">
 
                         @foreach(Auth::user()->characters as $character)
@@ -23,7 +25,7 @@
     <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" class="img-fluid rounded-circle z-depth-1"/>
         </div>
     <div class="text-center mt-3">
-    <h6 class="font-weight-bold pt-2"><a href="character/{{ $character->id }}">{{ $character->character_name }}</a></h6><span><a class="text-white btn btn-block btn-primary" href="character/{{ $character->id }}/edit" role="button">Link</a></span>
+    <h6 class="font-weight-bold pt-2"><a href="character/{{ $character->id }}">{{ $character->character_name }}</a></h6><span><a class="text-white btn btn-block btn-primary" href="character/{{ $character->id }}/edit" role="button">Edit This Character</a></span>
     </div>
     </div>
     </div>
